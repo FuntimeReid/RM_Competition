d35201c0f6ea80e7da08e586980f98616427c5d5
Comb1e <101026561+Comb1e@users.noreply.github.com>
2024-10-30 11:53:02 +0800

    CAN1发送

--- Task/Inc/CAN_Task.h
+++ Task/Inc/CAN_Task.h
@@ -6,5 +6,7 @@
 
 void CAN_Task();
 
+void CAN_SendMessage(CAN_HandleTypeDef *hcan,uint32_t StdId,int16_t message1,int16_t message2,int16_t message3,int16_t message4);
+
 #endif //CAN_TASK_H
 
--- Task/Src/CAN_Task.c
+++ Task/Src/CAN_Task.c
@@ -2,6 +2,9 @@
 #include "CAN_Task.h"
 #include "ChassisCtrl.h"
 #include "main.h"
+#include "can.h"
+
+uint8_t can_send_data[8];
 
 void CAN_Task()
 {
@@ -18,4 +21,28 @@ void CAN_Task()
             break;
         }
     }
+    CAN_SendMessage(&hcan1,0x200,0,0,1000,0);
+}
+
+void CAN_SendMessage(CAN_HandleTypeDef *hcan,uint32_t StdId,int16_t message1,int16_t message2,int16_t message3,int16_t message4)
+{
+    uint32_t send_mail_box;
+    CAN_TxHeaderTypeDef txHeader;
+
+    txHeader.StdId = StdId;
+    txHeader.IDE = CAN_ID_STD;
+    txHeader.DLC = 0x08;
+    txHeader.ExtId = 0x00;
+    txHeader.RTR = CAN_RTR_DATA;
+
+    can_send_data[0] = message1 >> 8;
+    can_send_data[1] = message1;
+    can_send_data[2] = message2 >> 8;
+    can_send_data[3] = message2;
+    can_send_data[4] = message3 >> 8;
+    can_send_data[5] = message3;
+    can_send_data[6] = message4 >> 8;
+    can_send_data[7] = message4;
+
+    HAL_CAN_AddTxMessage(hcan, &txHeader, can_send_data, &send_mail_box);
 }
